2a75a980f8c415d9bbd98c81f6e05a2f5a69f3b1
bhaiyaa <61397891+bhaiyaa@users.noreply.github.com>
2022-02-13 20:20:30 +0530

    Update mapla.js

--- pages/api/mapla.js
+++ pages/api/mapla.js
@@ -2,7 +2,7 @@ function handler(req, res) {
     
     const dany=req.headers['singam']
 
-    res.status(201).json({ message: 'Successfully stored message!',white:dany});
+    res.status(201).json({ message: 'Successfully stored message!',white:dany,mahesh:"babu"});
   
 }
 
