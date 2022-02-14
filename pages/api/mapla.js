a15717728b8d180cb2adc65a9adc6b9a7a5c05e4
bhaiyaa <61397891+bhaiyaa@users.noreply.github.com>
2022-02-14 10:15:40 +0530

    Update mapla.js

--- pages/api/mapla.js
+++ pages/api/mapla.js
@@ -1,4 +1,15 @@
+import NextCors from 'nextjs-cors';
 function handler(req, res) {
+    
+    await NextCors(req, res, {
+      // Options
+      methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+      origin: '*',
+      optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+   });
+
+    
+    
     
     const dany=req.headers['singam']
 
